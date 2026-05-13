fa799ab399d3a24af6e928e5e3c41070d257ff0a
walizada <walizada.official@gmail.com>
2026-05-13 13:22:19 +0200

    Build GitHub Pages deploy output

--- dist/server/assets/_tanstack-start-manifest_v-yOo9vCTJ.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/walizada/Documents/Projects/Novaris /novaris-web/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-BnOuamOK.js"] }, "/": { filePath: "/Users/walizada/Documents/Projects/Novaris /novaris-web/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BFcrRveK.js"] } }, clientEntry: "/assets/index-BnOuamOK.js" });
-export {
-  tsrStartManifest
-};
