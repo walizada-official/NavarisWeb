9d2d5f12b9f1fc5a35732db7e8bdfe331a8fa861
walizada <walizada.official@gmail.com>
2026-05-14 14:57:17 +0200

    add base for github

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/NovarisWeb/",
   plugins: [viteReact(), tailwindcss(), tsConfigPaths()],
 });
